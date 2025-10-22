0539ee36e177cf6956f34927913ce0e1a8f62fe8
sellkes <158508566+sellkes@users.noreply.github.com>
2025-10-22 13:41:19 -0700

    worked on PS

--- ps2_theoretical_ecology.R
+++ ps2_theoretical_ecology.R
@@ -135,7 +135,61 @@ gmean(rlambda)
 #(b) the population size over time expected if the deterministic value of λ were equal to the mean
 #observed geometric λ. (Set lwd=3 and col=‘red’ or ‘green’ to make these lines more visible).
 
+#a - λ were equal to the mean observed arithmetic λ
 plot(N[1,],log='y',ylim=c(1,10^20),type='n')
 for (r in 1:nrow(N)){
+  while{rlambda = 1.01
+  }
   lines(N[r,], col=Col)
 }
+
+#b deterministic value of λ were equal to the mean observed geometric λ
+plot(N[1,],log='y',ylim=c(1,10^20),type='n')
+for (r in 1:nrow(N)){
+  while{rlambda = gmean(rlambda)
+  }
+  lines(N[r,], col="green")
+}
+#the realizations were closer with the geometric lambda and not the arithmetic lambda, probably becuase this population is best 
+#described with a geometric function and so the geometric mean is more accurate
+
+nmean <- mean(N)
+nvar <- var(N)
+#the variance is really large (2.65) compared to the mean (1.17)
+
+rexpmean <- log(N[1]) + log(gmean(lambda))*TimeSteps
+
+hist(N)
+abline(v=nmean, col="purple") #realized mean
+abline(v=rexpmean, col = "darkgreen")
+
+#the expected mean is larger than the realized mean. I assume they'd get closer together with more repetitions / larger sample
+#size (bigger T)
+
+##Part B
+r_d <- 1
+K <- 100
+TimeSteps <- 50
+t <- c(1:TimeSteps)
+N <- numeric()
+N[1] = 9
+
+#B.2
+for (x in t)
+{
+  N[t+1] = N[t] + r_d*(1-(N[t]/K))*N[t]
+} 
+
+plot(N, log = "y")
+
+#making N(0) bigger makes the population reach K faster, and vice versa
+
+#I can't get much of my code to work so I am just now running the key code and interpreting the outputs, so sorry I didn't 
+#leave myself enough time for this problem set
+
+#Adding variation in R makes the population sizes less normal and causes them to bounce around a lot
+#adding stochastic temporal variation in K creates more noise (or chaos?) in the population - they all reach K around the same time, 
+#but adding even a little more variation changes the amplititude of the chaos and how often it's bouncing around. 
+#varying both K and r makes the dynamics more chaotic. 
+
+#
